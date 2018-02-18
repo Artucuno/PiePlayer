eb08fa24247ecd02978badd2347c3aecbe8cb692
Articuno1234 <Articuno1234@users.noreply.github.com>
2018-02-18 10:18:25 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import random
 # Song Playlist
 # Just put song name with the extention
 # eg : hello.mp3 or hi.wav
-songs = ["0", "A.mp3", "L.mp3", "M.mp3", "S.mp3"]
+songs = ["A.mp3", "L.wav", "M.ogg", "S.alsa"]
